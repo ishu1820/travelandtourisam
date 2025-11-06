cf0989c66646642642b82cd9b8db38984fa7bf3f
ishu1820 <ishitamahajan9449@gmail.com>
2025-11-06 16:53:04 +0530

    first page

---
+++ src/travelandtourism/Splash.java
@@ -0,0 +1,13 @@
+package travelandtourism;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Splash extends JFrame {
+    Splash(){
+        setSize(1200, 600);
+    }
+    public static void main(String[] args) {
+        new Splash();
+    }
+}
