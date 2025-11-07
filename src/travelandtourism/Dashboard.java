5f77c0eca59c56d18b8ca64a2261d38e4821a1c7
ishu1820 <ishitamahajan9449@gmail.com>
2025-11-07 19:31:38 +0530

    loading.java done

---
+++ src/travelandtourism/Dashboard.java
@@ -0,0 +1,36 @@
+package travelandtourism;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Dashboard extends JFrame {
+     Dashboard() {
+         //setBounds(0,0,1600,1000);
+         setExtendedState(JFrame.MAXIMIZED_BOTH);
+         setLayout(null);
+
+         JPanel p1 = new JPanel();
+         p1.setLayout(null);
+         p1.setBackground(new Color(0,0,102));
+         p1.setBounds(0,0,1600,65);
+         add(p1);
+
+         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/dashboard.png"));
+         Image i2 = i1.getImage().getScaledInstance(70, 70, Image.SCALE_DEFAULT);
+         ImageIcon i3 = new ImageIcon(i2);
+         JLabel icon = new JLabel(i3);
+         icon.setBounds(5,0, 70, 70);
+         p1.add(icon);
+
+         JLabel heading = new JLabel("Dashboard");
+         heading.setBounds(80, 10, 300, 40);
+         heading.setForeground(Color.WHITE);
+         heading.setFont(new Font("Tahoma", Font.BOLD, 19));
+         p1.add(heading);
+
+         setVisible(true);
+     }
+    public static void main(String[] args) {
+        new Dashboard();
+    }
+}
