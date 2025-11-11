e9dfa169c6c7cb99b38e35c5b957f1efa9f998ac
ishu1820 <ishitamahajan9449@gmail.com>
2025-11-11 17:22:47 +0530

    book package done

--- src/travelandtourism/BookPackage.java
+++ src/travelandtourism/BookPackage.java
@@ -11,178 +11,177 @@ import java.sql.*;
 import javax.swing.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-public class BookPackage extends JFrame {
-    private JPanel contentPane;
-    JTextField t1,t2;
-    Choice c1,c2,c3;
-    
-    public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    BookPackage frame = new BookPackage("");
-                    frame.setVisible(true);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
-
-    public BookPackage(String username) {
-        setBounds(420, 220, 1100, 450);
-        contentPane = new JPanel();
-        setContentPane(contentPane);
-        contentPane.setLayout(null);
-
-        ImageIcon i1  = new ImageIcon(ClassLoader.getSystemResource("icons/bookpackage.jpg"));
-        Image i3 = i1.getImage().getScaledInstance(500, 300,Image.SCALE_DEFAULT);
-        ImageIcon i2 = new ImageIcon(i3);
-        JLabel la1 = new JLabel(i2);
-        la1.setBounds(450,50,700,300);
-        add(la1);
-
-        JLabel lblName = new JLabel("BOOK PACKAGE");
-        lblName.setFont(new Font("Yu Mincho", Font.PLAIN, 20));
-        lblName.setBounds(118, 11, 300, 53);
-        contentPane.add(lblName);
-
-        JLabel la2 = new JLabel("Username :");
-        la2.setBounds(35, 70, 200, 14);
-        contentPane.add(la2);
-
-        JLabel l1 = new JLabel(username);
-        l1.setBounds(271, 70, 200, 14);
-        contentPane.add(l1);
-
-        JLabel lblId = new JLabel("Select Package :");
-        lblId.setBounds(35, 110, 200, 14);
-        contentPane.add(lblId);
-
-        c1 = new Choice();
-        c1.add("Gold Package");
-        c1.add("Silver Package");
-        c1.add("Bronze Package");
-        c1.setBounds(271, 110, 150, 30);
-        add(c1);
-
-        JLabel la3 = new JLabel("Total Persons");
-        la3.setBounds(35, 150, 200, 14);
-        contentPane.add(la3);
-
-        t1 = new JTextField();
-        t1.setText("0");
-        t1.setBounds(271, 150, 150, 20);
-        contentPane.add(t1);
-        t1.setColumns(10);
-
-        JLabel lbl1 = new JLabel("ID :");
-        lbl1.setBounds(35, 190, 200, 14);
-        contentPane.add(lbl1);
-
-        JLabel l2 = new JLabel();
-        l2.setBounds(271, 190, 200, 14);
-        contentPane.add(l2);
-
-        JLabel lbl2 = new JLabel("Number :");
-        lbl2.setBounds(35, 230, 200, 14);
-        contentPane.add(lbl2);
-
-        JLabel l3 = new JLabel();
-        l3.setBounds(271, 230, 200, 14);
-        contentPane.add(l3);
-
-        JLabel lbl3 = new JLabel("Phone :");
-        lbl3.setBounds(35, 270, 200, 14);
-        contentPane.add(lbl3);
-
-        JLabel l4 = new JLabel();
-        l4.setBounds(271, 270, 200, 14);
-        contentPane.add(l4);
+public class BookPackage extends JFrame implements ActionListener{
 
 
-        JLabel lblDeposite = new JLabel("Total Price :");
-        lblDeposite.setBounds(35, 310, 200, 14);
-        contentPane.add(lblDeposite);
+    Choice cpackage;
+    JTextField tfpersons;
+    String username;
+    JLabel labelusername, labelid, labelno, labelphone, labeltotal;
+    JButton checkprice, bookpackage, back;
+    BookPackage(String username){
+        this.username = username;
+        setBounds(350,200,900,500);
+        setLayout(null);
+        getContentPane().setBackground(Color.WHITE);
 
-        JLabel l5 = new JLabel();
-        l5.setBounds(271, 310, 200, 14);
-        l5.setForeground(Color.RED);
-        contentPane.add(l5);
+        JLabel text = new JLabel("BOOK PACKAGE");
+        text.setBounds(100,10,200,30);
+        text.setFont(new Font("Tahoma", Font.BOLD, 20));
+        add(text);
+
+        JLabel lblusername = new JLabel("Username");
+        lblusername.setBounds(40,70,100,20);
+        lblusername.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        add(lblusername);
+
+        labelusername = new JLabel();
+        labelusername.setBounds(250,70,100,20);
+        labelusername.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        add(labelusername);
+
+        JLabel lblpack = new JLabel("Select Package");
+        lblpack.setBounds(40,110,130,20);
+        lblpack.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        add(lblpack);
+
+        cpackage = new Choice();
+        cpackage.add("Gold Package");
+        cpackage.add("Silver Package");
+        cpackage.add("Bronze Package");
+        cpackage.setBounds(250, 110 , 100,20);
+        add(cpackage);
+
+        JLabel lblperson = new JLabel("Total Persons");
+        lblperson.setBounds(40,150,150,20);
+        lblperson.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        add(lblperson);
+
+        tfpersons = new JTextField("0");
+        tfpersons.setBounds(250,150,200,20);
+        add(tfpersons);
+
+        JLabel lblid = new JLabel("Id");
+        lblid.setBounds(40,190,150,20);
+        lblid.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        add(lblid);
+
+        labelid = new JLabel();
+        labelid.setBounds(250,190,200,20);
+        labelid.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        add(labelid);
+
+        JLabel lblno = new JLabel("Number");
+        lblno.setBounds(40,230,150,20);
+        lblno.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        add(lblno);
+
+        labelno = new JLabel();
+        labelno.setBounds(250,230,200,20);
+        labelno.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        add(labelno);
+
+        JLabel lblphone = new JLabel("Phone-no.");
+        lblphone.setBounds(40,270,150,20);
+        lblphone.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        add(lblphone);
+
+         labelphone = new JLabel();
+        labelphone.setBounds(250,270,200,20);
+        labelphone.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        add(labelphone);
+
+        JLabel lbltotal = new JLabel("Total Price");
+        lbltotal.setBounds(40,310,150,20);
+        lbltotal.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        add(lbltotal);
+
+        labeltotal = new JLabel();
+        labeltotal.setBounds(250,310,200,20);
+        labeltotal.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        add(labeltotal);
 
         try{
             Conn c = new Conn();
             ResultSet rs = c.s.executeQuery("select * from customer where username = '"+username+"'");
             while(rs.next()){
-                l2.setText(rs.getString("id_type"));
-                l3.setText(rs.getString("number"));
-                l4.setText(rs.getString("phone"));
-
+                labelusername.setText(rs.getString("username"));
+                labelid.setText(rs.getString("id"));
+                labelno.setText(rs.getString("number"));
+                labelphone.setText(rs.getString("phone"));
             }
 
             rs.close();
-        }catch(SQLException e){}
+        }catch(Exception e){
+            e.printStackTrace();
+        }
 
-        JButton b1 = new JButton("Check Price");
-        b1.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                String p = c1.getSelectedItem();
-                int cost = 0;
-                if(p.equals("Gold Package")) {
-                    cost += 12000;
-                }if(p.equals("Silver Package")){
-                    cost += 25000;
-                }else if(p.equals("Bronze Package")){
-                    cost += 32000;
-                }
 
-                cost *= Integer.parseInt(t1.getText());
-                l5.setText("Rs "+cost);
+        checkprice = new JButton("Check Price");
+        checkprice.addActionListener(this);
+        checkprice.setBounds(60, 380, 120, 25);
+        checkprice.setBackground(Color.BLACK);
+        checkprice.setForeground(Color.WHITE);
 
+        add(checkprice);
+
+
+        bookpackage = new JButton("Book Package");
+        bookpackage.setBounds(200,380,120,25);
+        bookpackage.setForeground(Color.WHITE);
+        bookpackage.setBackground(Color.BLACK);
+        bookpackage.addActionListener(this);
+        add(bookpackage);
+
+        back = new JButton("Back");
+        back.setBounds(340,380,120,25);
+        back.setForeground(Color.WHITE);
+        back.setBackground(Color.BLACK);
+        back.addActionListener(this);
+        add(back);
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/bookpackage.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(500, 300, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel icon = new JLabel(i3);
+        icon.setBounds(550,50, 500, 300);
+        add(icon);
+
+
+                setVisible(true);
+    }
+    public void actionPerformed(ActionEvent ae){
+        if(ae.getSource()==checkprice){
+            String pack = cpackage.getSelectedItem();
+            int cost = 0;
+            if(pack.equals("Gold Package")){
+                cost += 12000;
+            } else if (pack.equals("Silver Package")) {
+                cost += 25000;
+            }else{
+                cost += 32000;
             }
-        });
-        b1.setBounds(50, 350, 120, 30);
-        b1.setBackground(Color.BLACK);
-        b1.setForeground(Color.WHITE);
-        contentPane.add(b1);
+            int persons = Integer.parseInt(tfpersons.getText());
+            cost *= persons;
+            labeltotal.setText("Rs" + cost);
+        } else if (ae.getSource()==bookpackage) {
 
-
-        JButton btnNewButton = new JButton("Book");
-        btnNewButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
+            try{
                 Conn c = new Conn();
+                c.s.executeUpdate("insert into bookpackage values('"+labelusername.getText()+"', '"+cpackage.getSelectedItem()+"','"+tfpersons.getText()+"','"+labelid.getText()+"','"+labelno.getText()+"','"+labelphone.getText()+"','"+labeltotal.getText()+"')");
 
-
-                try{
-                    String s1 = c1.getSelectedItem();
-
-                    String q1 = "insert into bookPackage values('"+username+"', '"+c1.getSelectedItem()+"', '"+t1.getText()+"', '"+l2.getText()+"', '"+l3.getText()+"', '"+l4.getText()+"', '"+l5.getText()+"')";
-                    c.s.executeUpdate(q1);
-
-                    JOptionPane.showMessageDialog(null, "Package Booked Successfully");
-                    setVisible(false);
-                }catch(Exception ee){
-                    System.out.println(ee.getMessage());
-                }
-
-            }
-        });
-        btnNewButton.setBounds(200, 350, 120, 30);
-        btnNewButton.setBackground(Color.BLACK);
-        btnNewButton.setForeground(Color.WHITE);
-        contentPane.add(btnNewButton);
-
-        JButton btnExit = new JButton("Back");
-        btnExit.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null,"Package Booked Successfully");
                 setVisible(false);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        });
-        btnExit.setBounds(350, 350, 120, 30);
-        btnExit.setBackground(Color.BLACK);
-        btnExit.setForeground(Color.WHITE);
-        contentPane.add(btnExit);
-
-        getContentPane().setBackground(Color.WHITE);
+        }else {
+            setVisible(false);
+        }
+    }
+    public static void main(String[] args) {
+        new BookPackage("ishita11");
     }
 }
+
+
